f545295af5b275b54631d1cff6445ceea70e70c3
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-09 16:56:00 -0600

    Fix minor error with the id

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY,
-    name VARCHAR (100) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts INT NOT NULL,
-    neutered BOOLEAN NOT NULL,
-    weight_kg FLOAT NOT NULL,
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR (100) NOT NULL, 
+    date_of_birth DATE NOT NULL, 
+    escape_attempts INT NOT NULL, 
+    neutered BOOLEAN NOT NULL, 
+    weight_kg FLOAT NOT NULL, 
 );
 
 ALTER TABLE animals ADD species VARCHAR(100);
