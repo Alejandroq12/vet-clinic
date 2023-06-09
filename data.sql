08c9dcead2bd4398203954f67dac8132edd6366c
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-09 17:46:43 -0600

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -22,3 +22,5 @@ UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
