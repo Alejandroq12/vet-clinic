3f98df0d8047ff9dc3349356388f36b6b9526cbf
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-09 17:09:28 -0600

    Update schema to add foreign keys

--- schema.sql
+++ schema.sql
@@ -13,3 +13,11 @@ ALTER TABLE animals ADD species VARCHAR(100);
 
 CREATE TABLE owners (id SERIAL PRIMARY KEY, full_name VARCHAR(100) NOT NULL, age INT NOT NULL);
 CREATE TABLE species (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL);
+
+BEGIN;
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+COMMIT;
