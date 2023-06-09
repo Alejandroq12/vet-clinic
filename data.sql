d08c273dcc91dffbbe28ad47c23bd9e7c851efe0
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-09 17:47:07 -0600

    Add data into species table

--- data.sql
+++ data.sql
@@ -24,3 +24,4 @@ ROLLBACK;
 SELECT * FROM animals;
 
 INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
