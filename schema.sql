14ff3cb6da84c06817e9c19111781380df029918
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-10 01:01:23 -0600

    Create vets table

--- schema.sql
+++ schema.sql
@@ -21,3 +21,5 @@ ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
 COMMIT;
+
+CREATE TABLE vets (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, age INT NOT NULL, date_of_graduation DATE NOT NULL);
