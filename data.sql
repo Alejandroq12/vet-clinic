4474719b57a7cfd25055672d021bd646b06a14cd
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-10 02:47:27 -0600

    Update insert into animals table

--- data.sql
+++ data.sql
@@ -6,9 +6,6 @@ VALUES
   ('Gabumon', '2018-11-15', 2, TRUE, 8.0),
   ('Pikachu', '2021-01-07', 1, FALSE, 15.04),
   ('Devimon', '2017-05-12', 5, TRUE, 11.0);
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES
   ('Charmander', '2020-02-08', 0, false, -11),
   ('Plantmon', '2021-11-15', 2, true, -5.7),
   ('Squirtle', '1993-04-02', 3, false, -12.13),
