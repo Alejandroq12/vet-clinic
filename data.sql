009b7ea4bc2f0a1a33a7f98beadbe039a90888bf
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-10 01:15:16 -0600

    Add data to vets table

--- data.sql
+++ data.sql
@@ -40,3 +40,7 @@ UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WH
 UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon', 'Boarmon');
 COMMIT;
+
+BEGIN;
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '2000-04-23'), ('Maisy Smith', 26, '2019-01-17'), ('Stephanie Mendez', 64, '1981-05-04'), ('Jack Harkness', 38, '2008-06-08');
+COMMIT;
